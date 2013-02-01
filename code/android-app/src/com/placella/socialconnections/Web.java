256571b592fc39ea45dc8bf924158dd9df235163
Rouslan Placella <rouslan@placella.com>
2013-02-01 19:59:05 +0000

    Fixed WebView display

--- code/android-app/src/com/placella/socialconnections/Web.java
+++ code/android-app/src/com/placella/socialconnections/Web.java
@@ -4,21 +4,30 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Web extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web);
-        
-        WebView window = (WebView) findViewById(R.id.webView);
-        window.loadUrl(getString(R.string.webUrl));
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_web);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_web, menu);
-        return true;
-    }
+		WebView window = (WebView) findViewById(R.id.webView);
+		window.getSettings().setJavaScriptEnabled(true);
+		window.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url)
+			{
+				return false;
+			}
+		});
+		window.loadUrl(getString(R.string.webUrl) + "?mobile=true");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_web, menu);
+		return true;
+	}
 }

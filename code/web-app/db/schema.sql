ac28fcac47a79c5e902207f0d6d35762a94f21cf
Preslav Petkov <pres.petkov@gmail.com>
2013-02-13 17:55:04 +0000

    Updated web-app schema

--- code/web-app/db/schema.sql
+++ code/web-app/db/schema.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.0-alpha2
+-- version 3.5.5
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 03, 2013 at 03:03 PM
--- Server version: 5.5.29-0ubuntu0.12.10.1
--- PHP Version: 5.4.6-1ubuntu1.1
+-- Host: localhost
+-- Generation Time: Feb 13, 2013 at 05:49 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,10 +17,212 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `socialconnections`
+-- Database: `SocialConnections`
 --
-CREATE DATABASE `socialconnections` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `socialconnections`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(32) NOT NULL,
+  `lname` varchar(32) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `salt` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `fname`, `lname`, `username`, `email`, `password`, `salt`) VALUES
+(1, 'IT', 'Management', 'z87654321', '', '598a4553150f08602cf82422bbbb3b07', 'awxGwfzLNFuNx89EeLiQTsyFg5BnNP3Q');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assesment`
+--
+
+CREATE TABLE IF NOT EXISTS `assesment` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `gid` mediumint(9) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `weight` tinyint(4) NOT NULL COMMENT 'Percentage of total marks for the module',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `composite` (`id`,`gid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `sid` mediumint(9) NOT NULL,
+  `gid` mediumint(9) NOT NULL,
+  `isLecture` tinyint(1) NOT NULL DEFAULT '1',
+  `present` tinyint(1) NOT NULL DEFAULT '0',
+  `excuse` text,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `excuse_viewed` tinyint(1) NOT NULL DEFAULT '0',
+  UNIQUE KEY `composite` (`sid`,`gid`,`time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE IF NOT EXISTS `class` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `did` mediumint(9) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `composite` (`id`,`did`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE IF NOT EXISTS `department` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `headId` mediumint(9) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE IF NOT EXISTS `group` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `moid` mediumint(9) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `moid` (`moid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_student`
+--
+
+CREATE TABLE IF NOT EXISTS `group_student` (
+  `gid` mediumint(9) NOT NULL,
+  `sid` mediumint(9) NOT NULL,
+  PRIMARY KEY (`gid`,`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lecturer`
+--
+
+CREATE TABLE IF NOT EXISTS `lecturer` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(32) NOT NULL,
+  `lname` varchar(32) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `salt` varchar(32) NOT NULL,
+  `did` mediumint(9) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `lecturer`
+--
+
+INSERT INTO `lecturer` (`id`, `fname`, `lname`, `username`, `email`, `password`, `salt`) VALUES
+(1, 'Mary', 'Davin', 'r12345678', 'mary.davin@cit.ie', '4265f0910b86350037ead6662d51699c', 'jYyPaiH52KxdCDnpPZ0eopACCFeFkbdT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `module`
+--
+
+CREATE TABLE IF NOT EXISTS `module` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `credits` int(11) NOT NULL,
+  `CRN` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `moduleoffering`
+--
+
+CREATE TABLE IF NOT EXISTS `moduleoffering` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `mid` mediumint(9) NOT NULL,
+  `year` year(4) NOT NULL,
+  `term` tinyint(4) NOT NULL,
+  UNIQUE KEY `composite` (`id`,`mid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `moduleoffering_lecturer`
+--
+
+CREATE TABLE IF NOT EXISTS `moduleoffering_lecturer` (
+  `moid` mediumint(9) NOT NULL,
+  `lid` mediumint(9) NOT NULL,
+  UNIQUE KEY `composite` (`moid`,`lid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE IF NOT EXISTS `notes` (
+  `url` varchar(256) NOT NULL,
+  `gid` mediumint(9) NOT NULL,
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `composite` (`gid`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `results`
+--
+
+CREATE TABLE IF NOT EXISTS `results` (
+  `aid` mediumint(9) NOT NULL,
+  `sid` mediumint(9) NOT NULL,
+  `grade` tinyint(4) NOT NULL,
+  `isDraft` tinyint(1) NOT NULL,
+  UNIQUE KEY `composite` (`aid`,`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -37,6 +239,35 @@ CREATE TABLE IF NOT EXISTS `speedlimit` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(32) NOT NULL,
+  `lname` varchar(32) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `salt` varchar(32) NOT NULL,
+  `cid` mediumint(9) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`email`),
+  UNIQUE KEY `composite` (`id`,`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `fname`, `lname`, `username`, `email`, `password`, `salt`) VALUES
+(1, 'Gary', 'Brady', 'r00012345', 'gary.brady@mycit.ie', '1ceafbd665b7bccfef45ae8e573335da', 'IrUN710qV0GG6HmPOj984eIrwFcC4OCK'),
+(2, 'Preslav', 'Petkov', 'r00073209', 'preslav.petkov@mycit.ie', '8471cd440dfd9cf75402d82c8b820ed9', '1DYrhbckFt5uVB5n9N6lqLN7ppxKkwMo'),
+(3, 'Rouslan', 'Placella', 'r00077389', 'rouslan.placella@mycit.ie', '25318b182c631b6995b7268c3251aab8', 'NqK3zDnUQAeh6pR3DHFV4NCjKLdT6CRp');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `token`
 --
 
@@ -48,36 +279,79 @@ CREATE TABLE IF NOT EXISTS `token` (
   KEY `expires` (`expires`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Remote Authentication Tokens';
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `user`
+-- Constraints for dumped tables
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `uid` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `fname` varchar(32) NOT NULL,
-  `lname` varchar(32) NOT NULL,
-  `username` varchar(64) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `salt` varchar(32) NOT NULL,
-  `type` tinyint(4) NOT NULL,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `username` (`username`,`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='System Users' AUTO_INCREMENT=7 ;
+--
+-- Constraints for table `assesment`
+--
+ALTER TABLE `assesment`
+  ADD CONSTRAINT `assesment_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Dumping data for table `user`
+-- Constraints for table `attendance`
 --
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_2` FOREIGN KEY (`gid`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `attendance_ibfk_3` FOREIGN KEY (`sid`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-INSERT INTO `user` (`uid`, `fname`, `lname`, `username`, `email`, `password`, `salt`, `type`) VALUES
-(1, 'Super', 'User', 'root', '', 'b5b4468dd1b6fccfd1dbdea6de82cae0', 'bPvg039ulLLZQWvxGFaDW7Sjy8Da69Eh', 4),
-(2, 'Rouslan', 'Placella', 'r00077389', 'rouslan.placella@mycit.ie', '25318b182c631b6995b7268c3251aab8', 'NqK3zDnUQAeh6pR3DHFV4NCjKLdT6CRp', 1),
-(3, 'Mary', 'Davin', 'r12345678', 'mary.davin@cit.ie', '4265f0910b86350037ead6662d51699c', 'jYyPaiH52KxdCDnpPZ0eopACCFeFkbdT', 2),
-(4, 'Gary', 'Brady', 'r00012345', 'gary.brady@mycit.ie', '1ceafbd665b7bccfef45ae8e573335da', 'IrUN710qV0GG6HmPOj984eIrwFcC4OCK', 1),
-(5, 'Preslav', 'Petkov', 'r00072727', 'preslav.petkov@mycit.ie', '8471cd440dfd9cf75402d82c8b820ed9', '1DYrhbckFt5uVB5n9N6lqLN7ppxKkwMo', 1),
-(6, 'IT', 'Management', 'z87654321', '', '598a4553150f08602cf82422bbbb3b07', 'awxGwfzLNFuNx89EeLiQTsyFg5BnNP3Q', 3);
+--
+-- Constraints for table `class`
+--
+ALTER TABLE `class`
+  ADD CONSTRAINT `class_ibfk_1` FOREIGN KEY (`did`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `group`
+--
+ALTER TABLE `group`
+  ADD CONSTRAINT `group_ibfk_1` FOREIGN KEY (`moid`) REFERENCES `moduleoffering` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `group_student`
+--
+ALTER TABLE `group_student`
+  ADD CONSTRAINT `group_student_ibfk_2` FOREIGN KEY (`sid`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `group_student_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `lecturer`
+--
+ALTER TABLE `lecturer`
+  ADD CONSTRAINT `lecturer_ibfk_1` FOREIGN KEY (`did`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `moduleoffering`
+--
+ALTER TABLE `moduleoffering`
+  ADD CONSTRAINT `moduleoffering_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `module` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `moduleoffering_lecturer`
+--
+ALTER TABLE `moduleoffering_lecturer`
+  ADD CONSTRAINT `moduleoffering_lecturer_ibfk_1` FOREIGN KEY (`moid`) REFERENCES `moduleoffering` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `moduleoffering_lecturer_ibfk_2` FOREIGN KEY (`lid`) REFERENCES `lecturer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `results`
+--
+ALTER TABLE `results`
+  ADD CONSTRAINT `results_ibfk_2` FOREIGN KEY (`aid`) REFERENCES `assesment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `results_ibfk_3` FOREIGN KEY (`sid`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

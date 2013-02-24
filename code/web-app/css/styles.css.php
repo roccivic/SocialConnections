40019b1c84a3c73d37460ec794367c7f4a380010
Rouslan Placella <rouslan@placella.com>
2013-02-24 19:31:34 +0000

    Web-app: set future 'expires' headers for static content

--- code/web-app/css/styles.css.php
+++ code/web-app/css/styles.css.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/css; charset=UTF-8');
-header('Expires: ' . gmdate('D, d M Y H:i:s', time() + (3600 * 24)) . ' GMT');
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + (3600 * 24 * 30)) . ' GMT');
 
 foreach (scandir('.') as $value) {
 	if (preg_match('@\.css$@', $value)) {

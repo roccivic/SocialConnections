7edfcda37984d09b5bc28560e43407580294fc3d
Gary <Brady>
2013-02-28 20:19:46 +0000

    Web-app: viewclass(manageClass)

--- code/web-app/classes/pages/Page_manageClasses.class.php
+++ code/web-app/classes/pages/Page_manageClasses.class.php
@@ -27,6 +27,19 @@ class Page_manageClasses extends Page_selectDepartment {
 	public function display($did) 
 	{
 		$did = intval($did);
+
+		$cid = 0;		
+		if(!empty($_REQUEST['cid'])){
+			$cid = $_REQUEST['cid'];
+		}
+		//is this not correct
+		if (!empty($_REQUEST['view']) ) 
+		{			
+			$did = $_REQUEST['did'];
+			$cid = $_REQUEST['cid'];
+			$this->viewClass($did,$cid);			
+		}
+	
 		
 		$this->displayClasses($did);
 	}
@@ -86,11 +99,11 @@ class Page_manageClasses extends Page_selectDepartment {
 	 *
 	 * @return void
 	 */
-	private function printClassesListItem($cid, $did, $name)
+	private function printClassesListItem($did, $cid, $name)
 	{
         $this->addHtml(
 	        sprintf(
-	        	'<li><a href="?action=manageClasses&cid=%d&did=%d&view=1">%s</a></li>',
+	        	'<li><a href="?action=manageClasses&did=%d&cid=%d&view=1">%s</a></li>',
 	        	$cid,
 	        	$did,
 	        	$name
@@ -105,5 +118,69 @@ class Page_manageClasses extends Page_selectDepartment {
 	private function printClassesListFooter()
 	{
         $this->addHtml('</ul>');
+	}
+		/**
+	 * Prints the details of a lecturer
+	 *
+	 * @return void
+	 */
+	private function viewClass($did,$cid)
+	{
+		$details = $this->getClassDetails($did, $cid);
+		if (isset($details['cid']));
+		$html = '';
+		$html .= '<h2>' . $details['name'] . '</h2>';
+		$html .=  __('Department: ');
+		$html .= $this->getDepartmentName($did);
+		$html .= '<br/><br/>';
+		$html .= '<a href="?action=manageLecturers&did='.$did.'&cid='.$cid.'&editForm=1"';
+    	$html .= ' data-role="button" data-theme="b">';
+    	$html .= __('Edit') . '</a>';
+    	$html .= '<a href="?action=manageLecturers&did='.$did.'&cid='.$cid.'&delete=1"';
+    	$html .= ' data-role="button" data-theme="b">';
+    	$html .= __('Delete') . '</a>';
+    	$this->addHtml($html);
+    	
+	}
+		/**
+	 * Returns an array of a classes details
+	 *
+	 * @return array
+	 */
+	private function getClassDetails($did, $cid)
+	{
+		//$arr = array();
+		$db = Db::getLink();
+		$stmt = $db->prepare(
+			"SELECT `id`, `name`, `did` FROM `class` where `id` = ?"
+		);		
+		$stmt->bind_param('i', $cid);
+		$stmt->execute();
+		$stmt->bind_result($id, $name, $did);
+		$stmt->fetch();
+		$stmt->close();
+		return array(
+			'cid' => $id,
+			'name' => $name,		
+			'did' => $did
+		);
+	}
+	/**
+	 * Returns the name of a department given its id
+	 *
+	 * @return string
+	 */
+	private function getDepartmentName($did)
+	{
+		$db = Db::getLink();
+		$stmt = $db->prepare(
+			"SELECT `name` FROM `department` WHERE `id` = ?;"
+		);
+		$stmt->bind_param('i', $did);
+		$stmt->execute();
+		$stmt->bind_result($name);
+		$stmt->fetch();
+		$stmt->close();
+		return $name;
 	}
 }

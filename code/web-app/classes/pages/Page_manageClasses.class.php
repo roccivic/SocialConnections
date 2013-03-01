aedd003338579f83f2ec982d022df74a4269ffc0
Gary <Brady>
2013-03-01 13:13:48 +0000

    Web-app : manageClass

--- code/web-app/classes/pages/Page_manageClasses.class.php
+++ code/web-app/classes/pages/Page_manageClasses.class.php
@@ -32,13 +32,86 @@ class Page_manageClasses extends Page_selectDepartment {
 		if(!empty($_REQUEST['cid'])){
 			$cid = $_REQUEST['cid'];
 		}
-		//is this not correct
+		$name = '';
+		if(!empty($_REQUEST['name'])){
+			$name = $_REQUEST['name'];
+		}
 		if (!empty($_REQUEST['view']) ) 
 		{			
 			$did = $_REQUEST['did'];
 			$cid = $_REQUEST['cid'];
 			$this->viewClass($did,$cid);			
+		}else if (! empty($_REQUEST['delete'])) 
+		{
+			$this->deleteClass($cid);
+			
+		} else if (! empty($_REQUEST['edit'])) {
+			if ($this->validateForm(false, $cid, $name, $did )
+				&& $this->updateClass($name, $cid)
+			) {
+				$this->addNotification(
+					'notice',
+					__('The class details were successfully updated.')
+				);
+				
+			} else {
+				$this->addNotification(
+					'error',
+					__('An error occured while processing your request.')
+				);
+				$details = $this->getClasses($did);
+				$name = $details['name'];
+				$this->editClassForm($did,$cid, $name);
+			}
+		}else if (! empty($_REQUEST['create'])) 
+			{
+			if ($this->validateForm(true, $cid, $name, $did)
+				&& $this->createClass($name, $did)) 
+			{
+				$this->addNotification(
+					'notice',
+					__('The Group was successfully created.')
+				);
+				
+			} else {
+				$this->addNotification(
+					'error',
+					__('An error occured while processing your request.')
+				);
+				$this->editClassForm($did,$cid, $name);
+			}
+		}else if (! empty($_REQUEST['editForm'])) {
+			$details = $this->getClassDetails($did, $cid);
+			$name = $details['name'];
+			if ($cid > 0 && empty($name)) {
+				$this->addNotification(
+					'error',
+					__('The selected class does not exist')
+				);
+				
+			} else {
+				$this->editClassForm($did,$cid, $name);
+			}
+		}else if (! empty($_REQUEST['delete'])) 
+		{
+			$cid = $_REQUEST['cid'];
+			if($this->deleteClass($cid)){
+				$this->addNotification(
+					'notice',
+					__('The selected class does not exist')
+				);
+			}else{
+				$this->addNotification(
+					'error',
+					__('The selected class does not exist')
+				);
+			}
+			
+			
+			
 		}
+
+		
 	
 		
 		$this->displayClasses($did);
@@ -84,7 +157,7 @@ class Page_manageClasses extends Page_selectDepartment {
 	private function printClassesListHeader($did)
 	{
 		$html = '';
-    	$html .= '<a href="?action=manageClasses&did='.$did.'&editForm=1"';
+    	$html .= '<a href="?action=manageClasses&did='.$did.'&create=1"';
     	$html .= ' data-role="button" data-theme="b">';
     	$html .= __('Create Class') . '</a>';
         $html .= '<ul data-role="listview" data-divider-theme="b" ';
@@ -133,10 +206,10 @@ class Page_manageClasses extends Page_selectDepartment {
 		$html .=  __('Department: ');
 		$html .= $this->getDepartmentName($did);
 		$html .= '<br/><br/>';
-		$html .= '<a href="?action=manageLecturers&did='.$did.'&cid='.$cid.'&editForm=1"';
+		$html .= '<a href="?action=manageClasses&did='.$did.'&cid='.$cid.'&editForm=1"';
     	$html .= ' data-role="button" data-theme="b">';
     	$html .= __('Edit') . '</a>';
-    	$html .= '<a href="?action=manageLecturers&did='.$did.'&cid='.$cid.'&delete=1"';
+    	$html .= '<a href="?action=manageClasses&did='.$did.'&cid='.$cid.'&delete=1"';
     	$html .= ' data-role="button" data-theme="b">';
     	$html .= __('Delete') . '</a>';
     	$this->addHtml($html);
@@ -182,5 +255,125 @@ class Page_manageClasses extends Page_selectDepartment {
 		$stmt->fetch();
 		$stmt->close();
 		return $name;
+	}
+	/**
+	 * Displays a form for editing a class
+	 *
+	 * @return void
+	 */
+
+	
+	private function editClassForm($did, $cid, $name)
+	{
+		$html = '<form method="post" action="">';
+		if($cid == 0) {
+			$html .= '<h3>' . __('Create Class') . '</h3>';
+			$html .= '<input name="create" value="1" type="hidden" />';
+		} else {
+			$html .= '<h3>' . __('Edit Group') . '</h3>';
+			$html .= '<input name="edit" value="1" type="hidden" />';
+			$html .= '<input name="cid" value="'.$cid.'" type="hidden" />';
+		}
+		
+		$html .= '<div data-role="fieldcontain">';
+		$html .= '<label for="name">' . __('Name') . ': </label>';
+		$html .= '<input type="text" name="name" id="name" ';
+		$html .= 'value="' . htmlspecialchars($name) . '" />';
+		$html .= '</div>';
+		$html .= '<input data-theme="b" type="submit" value="' . __('Save') . '" />';
+		$html .= '</form>';
+		$this->addHtml($html);
+	}
+	/**
+	 * Creates a new class
+	 *
+	 * @return bool success
+	 */
+	private function createClass($name, $did) {
+		$db = Db::getLink();
+		$stmt = $db->prepare(
+			"INSERT INTO class (name, did) VALUES(?, ?);"
+		);
+		$stmt->bind_param('si', $name, $did);
+		$success = $stmt->execute();
+		$stmt->close();
+		return $success;
+	}
+
+	/**
+	 * Deletes a Class
+	 *
+	 * @return bool success
+	 */
+		private function deleteClass($cid) 
+	{
+		$db = Db::getLink();
+		$stmt = $db->prepare(
+			"DELETE FROM `class` WHERE `id`=?;"
+		);
+		$stmt->bind_param("i", $cid);
+		$success = $stmt->execute();
+		$stmt->close();
+		return $success;
+	}
+	/**
+	 * Updates the class details
+	 *
+	 * @return bool success
+	 */
+	private function updateClass($name, $cid) {
+		$db = Db::getLink();
+		$db->query("SET AUTOCOMMIT=0");
+		$db->query("START TRANSACTION");
+		$stmt = $db->prepare(
+			"UPDATE `class` SET `name` = ? WHERE `id` = ?;"
+		);
+		$stmt->bind_param('si', $name, $cid);
+		$success = $stmt->execute();
+		$stmt->close();
+		
+		if($success) {
+			$db->query("COMMIT");
+		}else {
+			$db->query("ROLLBACK");
+		}
+		return $success;
+	}
+	/**
+	 * Checks if the form details for editing/creating
+	 * a group are valid
+	 *
+	 * @return bool
+	 */
+	private function validateForm($isCreate, $cid, $name, $did)
+	{
+		$success = true;
+		if (! $isCreate && $cid < 1) {
+			$success = false;
+			$this->addNotification(
+				'warning',
+				__('Invalid group selected')
+			);
+		}elseif(! $isCreate && $did < 1) {
+			$success = false;
+			$this->addNotification(
+				'warning',
+				__('Invalid department selected')
+			);
+		} else if (strlen($name) > 64) {
+			$success = false;
+			$this->addNotification(
+				'warning',
+				__('Class name must be 64 characters long or less.')
+			);
+		} else if (strlen($name) < 1) {
+			$success = false;
+			$this->addNotification(
+				'warning',
+				__('Class name must be at least 1 character long.')
+			);
+		} 
+		
+		return $success;
 	}
 }

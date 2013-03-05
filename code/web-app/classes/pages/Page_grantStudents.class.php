dd06377e594085b1124a804ad4e3071f6a67847e
Gary <Brady>
2013-03-05 00:19:33 +0000

    web-app: Grant students working( not printing)
    and fixed addition duplicate menus

--- code/web-app/classes/pages/Page_grantStudents.class.php
+++ code/web-app/classes/pages/Page_grantStudents.class.php
@@ -28,43 +28,69 @@ class Page_grantStudents extends Page_selectDepartment {
 	{
 		$did = intval($did);
 
-		$this->displayGrantStudents();
 		
+
+		if (!empty($_REQUEST['did']))
+		{
+			$dname = $this->getDepartmentName($did);
+			if(!empty($dname))
+			{
+				$this->displayGrantStudents($did);
+			}else{
+
+				$this->addNotification(
+					'warning',
+					__('Department does not exist.')
+				);
+				$this->departmentSelector();
+		
+			}
+		}
 	}
-	
-	
-	/**
-	 * Calls print functions for classes
-	 *
+/**
+	 * Prints the students who get the grant
+	 *and how much they are owed
 	 * @return void
 	 */
-	private function displayGrantStudents()
-	{
-		$students = $this->getGrantStudentsDetails();
-		$html ='';
-		foreach($students as $item) {
-			$html .= $students['name'];
-			$html .= $students['grantOwed'];
-		}
-		
-		$this->addHtml($html);
+	private function displayGrantStudents($did)
+	{		
+
+    		$html  = '<ul data-role="listview" data-divider-theme="b" data-inset="true">';
+	        $html .= '<li data-role="list-divider" role="heading">';
+	        $html .= __('Grant Amounts Owed');
+	        $html .= '</li>';
+	       
+	        $grants = $this->getGrantStudentsDetails($did);
+	        $details = array(
+	        	__('') => $grants['name'],
+	        	__('€') => $grants['grantOwed'],
+	        	
+	        );
+
+	        foreach ($details as $key => $value) {
+		    	$html .= '<li>';
+		        $html .= $key.$value;
+		        		        
+		        $html .= '</li>';
+	    	}
+
+	        $html .= '</ul>';
+	        $this->addHtml($html);
 	}
 
-	
-		/**
-	 * Returns an array of classes details
-	 *
+	/**
+	 * Returns an array of students who get a grant 
+	 *and how much they are owed given a department
 	 * @return array
 	 */
-	private function getGrantStudentsDetails()
+	private function getGrantStudentsDetails($did)
 	{
 		$arr = array();
 		$db = Db::getLink();
 		$stmt = $db->prepare(
-			"SELECT `fname`, `lname`, `grantOwed`  FROM `student` where `hasGrant` = 1 "
-		);
-		
-		//$stmt->bind_param('i', $did);
+	"SELECT `fname`, `lname`, `grantOwed` FROM `student` LEFT JOIN `class` ON `student`.`cid` = `class`.`id` LEFT JOIN `department` ON `department`.`id` = `class`.`did` WHERE `student`.`hasGrant` = 1 AND `department`.`id` = ?"
+	);
+		$stmt->bind_param('i', $did);
 		$stmt->execute();
 		$stmt->bind_result($fname,$lname, $grantOwed);
 		$stmt->fetch();
@@ -75,25 +101,30 @@ class Page_grantStudents extends Page_selectDepartment {
 			
 		);
 	}
+
 		/**
-	 * Returns an array of classes details
+	 * Returns the name of a department given its id
 	 *
-	 * @return array
+	 * @return string
 	 */
-	private function countGrantStudents()
+	private function getDepartmentName($did)
 	{
-		$arr = array();
 		$db = Db::getLink();
 		$stmt = $db->prepare(
-			"SELECT COUNT('hasGrant') FROM `student` WHERE `hasGrant` = 1;"
+			"SELECT `name` FROM `department` WHERE `id` = ?;"
 		);
-		
-		//$stmt->bind_param('i', $did);
+		$stmt->bind_param('i', $did);
 		$stmt->execute();
-		$stmt->bind_result($grantStudent);
+		$stmt->bind_result($name);
 		$stmt->fetch();
 		$stmt->close();
-		return $grantStudent;
+		return $name;
 	}
 	
 }
+
+
+
+
+
+
